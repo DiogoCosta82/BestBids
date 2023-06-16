298c18d6dd37a77c9161b145723e1e4182af8328
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-06-16 16:38:32 +0200

    7 commit

--- annonce.class.php
+++ annonce.class.php
@@ -3,11 +3,27 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="Style/style.css" />
+    <title>Best Bid's - Page annonces</title>
 </head>
 
 <body>
+    <header>
+        <?php
+        include __DIR__ . '/Nav/menu.php';
+
+
+        $menu_liens = [
+            '/BestBids/index.php' => 'Accueil',
+            '/BestBids/logout.php' => 'Déconnexion',
+
+        ];
+
+        afficher_menu("", $menu_liens);
+        ?>
+    </header>
 
     <div class="bodyPage"></div>
 
--- login.php
+++ login.php
@@ -1,57 +1,6 @@
 <?php
+require_once(__DIR__ . '/Class/login.class.php'); ?>
 
-// Fonction de LOGIN
-function Login($email, $password)
-{
-  // Démarrage de session
-  session_start();
-
-  // Vérifier si l'utilisateur est déjà connecté
-  if (isset($_SESSION["user"])) {
-    // Rediriger vers la page d'accueil
-    header("Location: index.php");
-    exit;
-  }
-
-  // Vérifier les informations de connexion
-  try {
-    try {
-      $dbh = new PDO("mysql:dbname=best_bids;host=127.0.0.1", "root", "");
-    } catch (Exception $e) {
-      $bdh = new PDO("mysql:dbname=best_bids;host=127.0.0.1;port=8889", "root", "root");
-    }
-
-    $query = $dbh->prepare('SELECT * FROM user WHERE `email` = :email AND `password` = :password');
-    $query->execute(array(':email' => $email, ':password' => $password));
-    $results = $query->fetch(); // Récupération des données
-
-    if ($results) {
-      // Stocker les données de l'utilisateur dans la session
-      $_SESSION["user"] = $results;
-      // Rediriger vers la page utilisateur connecté
-      header("Location: annonces.php");
-      exit;
-    } else {
-      // Rediriger vers la page de connexion avec un message d'erreur
-      header("Location: login.php?error=1");
-      exit;
-    }
-  } catch (PDOException $e) {
-    echo "Une erreur s'est produite lors de la requête. Veuillez contacter l'administrateur du système. <br><br> Erreur : " . $e->getMessage();
-    die();
-  }
-}
-
-// Vérifier si le formulaire de connexion a été soumis
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Récupérer les données du formulaire
-  $email = $_POST["email"];
-  $password = $_POST["password"];
-
-  // Appeler la fonction SignIn avec les données de connexion
-  Login($email, $password);
-}
-?>
 
 <!DOCTYPE html>
 <html>
